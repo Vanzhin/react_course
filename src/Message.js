5b779c407a397f9300c4cfb558ad5965b7ca2ea8
nikolay vanzhin <nikolay.vanzhin@yandex.ru>
2022-09-16 14:39:16 +0500

    hw1

---
+++ src/Message.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Message({name}) {
+    return (
+        <div className={"example App-logo"}>
+            <h2>Hello, {name}</h2>
+        </div>
+    );
+}
+
+export default Message;
