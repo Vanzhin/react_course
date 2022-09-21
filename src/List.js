c6873132c91afeb3a5d6ed48c010f71df48ef707
nikolay vanzhin <nikolay.vanzhin@yandex.ru>
2022-09-21 15:06:41 +0500

    hw3

--- src/List.js
+++ src/List.js
@@ -1,21 +1,56 @@
 import React from 'react';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemText from '@mui/material/ListItemText';
+import ListItemAvatar from '@mui/material/ListItemAvatar';
+import Avatar from '@mui/material/Avatar';
+import Typography from '@mui/material/Typography';
+import { useTheme } from '@mui/material/styles';
 
-function List({array}) {
-    if (array.length>0){
+
+function MessageList({array}) {
+    const theme = useTheme();
+
+    if (array.length > 0) {
         return (
-            <div className={"showBlue"}>
-                <ul>
-                    {array.map(item => (
-                        <li>
-                            <p>{item.user} пишет: </p>
-                            <span>{item.text}</span>
-                        </li>
+            <List sx={{width: '100%', maxWidth: 360, bgcolor: theme.palette.secondary.background, borderRadius: 2}}>
+                {array.map(item => (
+                    <ListItem alignItems="flex-start" key={item.id}>
+                        <ListItemAvatar>
+                            <Avatar alt={item.user.toUpperCase()} src="/static/images/avatar/1.jpg"/>
+                        </ListItemAvatar>
+                        <ListItemText
+                            primary={
+                                <React.Fragment>
+                                    <Typography
+                                        sx={{display: 'inline', color: 'black'}}
+                                        component="span"
+                                        variant="body1"
+                                        color="text.primary"
+                                    >
+                                        {item.user} wrote
+                                    </Typography>
+
+                                    <Typography
+                                        sx={{display: 'block', color: 'black'}}
+                                        component="div"
+                                        variant="body1"
+                                        color="text.primary"
+                                    >
+                                        {item.text}
+                                    </Typography>
+
+                                </React.Fragment>
+                            }
+
+                        />
+                    </ListItem>
                     ))}
-                </ul>
-            </div>
+            </List>
+
         );
     }
 
 }
 
-export default List;
+export default MessageList;
